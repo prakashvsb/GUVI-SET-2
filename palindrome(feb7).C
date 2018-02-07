daf1c59add6ef33e7bca4e27f528492bb68641f1
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-02-07 15:51:12 +0530

    Create palindrome(feb7).C

---
+++ palindrome(feb7).C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,r,a,sum=0;
+	scanf("%d",&n);
+	a=n;
+  while(a!=0)
+{
+	r=a%10;
+	sum=sum*10+(r);
+	a=a/10;
+}
+  if(n==sum)
+{
+	printf("Yes,The Number is Palindrome");
+}
+else
+{
+	printf("No,The Number is Not Palindrome");
+}
+	return 0;
+}
