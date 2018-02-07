259cc2366ce497190bd5f016a9ca043f12239682
prakashvsb <35994953+prakashvsb@users.noreply.github.com>
2018-02-07 16:43:51 +0530

    Create evennumbers.C

---
+++ evennumbers.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a;
+clrscr();
+
+printf("\n Enter the number");
+scanf("%d",&a);
+for(i=0;i<=a;i++)
+{
+if(i%2==0)
+ {
+  printf("%d",i);
+ }
+}
+return 0;
+}
